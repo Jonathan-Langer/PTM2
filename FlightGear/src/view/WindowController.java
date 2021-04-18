70c68b3260c5abae14d435e302d7d156a99394d1
dror <dror@LAPTOP-UN6L3D3P>
2021-04-18 16:52:12 +0300

    add combo box options

--- FlightGear/src/view/WindowController.java
+++ FlightGear/src/view/WindowController.java
@@ -1,12 +1,26 @@
 package view;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.stage.FileChooser;
 
-public class WindowController {
+public class WindowController implements Initializable{
 	
 	String csvFileName;
+	@FXML
+	private ComboBox<String> options;
+	
+	public WindowController() {
+		//options=new ComboBox<>();
+		//options.setValue("1");
+	}
 	
 	public void openCSVFile() {
 		FileChooser fc=new FileChooser();
@@ -18,5 +32,13 @@ public class WindowController {
 		File chooser=fc.showOpenDialog(null);
 		if(chooser!=null)
 			csvFileName=chooser.getName();
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		ObservableList<String> list=FXCollections.observableArrayList
+				("0.25","0.5","0.75","1","1.25","1.5","1.75","2");
+		options.setItems(list);
+		options.setValue("1");
 	}
 }
