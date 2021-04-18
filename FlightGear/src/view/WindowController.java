f306a7e8a761c60862dcff50706477400ec752a2
dror <dror@LAPTOP-UN6L3D3P>
2021-04-18 15:32:14 +0300

    add file chooser in button "open"

--- FlightGear/src/view/WindowController.java
+++ FlightGear/src/view/WindowController.java
@@ -1,5 +1,22 @@
 package view;
 
+import java.io.File;
+
+import javafx.stage.FileChooser;
+
 public class WindowController {
 	
+	String csvFileName;
+	
+	public void openCSVFile() {
+		FileChooser fc=new FileChooser();
+		fc.setTitle("open csv file");
+		fc.setInitialDirectory(new File("./resources"));
+		fc.getExtensionFilters().addAll(
+				new FileChooser.ExtensionFilter("csv file", "*.csv")
+				);
+		File chooser=fc.showOpenDialog(null);
+		if(chooser!=null)
+			csvFileName=chooser.getName();
+	}
 }
